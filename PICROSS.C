425e2e3dfe015f8f6696196c82653c61f8e38ac8
Hitchiro93 <103576867+Hitchiro93@users.noreply.github.com>
2022-04-13 14:05:58 +0200

    Update PICROSS.C

--- PICROSS.C
+++ PICROSS.C
@@ -1,1 +1,10 @@
 #include<stdio.h>
+
+/*Initialise un tableau d'une dimension avec une valeur*/
+void initArray1D(int taille,int iArray[taille], int valeur)
+{
+	for(int i = 0; i < taille; i++)
+	{
+		iArray[i] = valeur;
+	}
+}
